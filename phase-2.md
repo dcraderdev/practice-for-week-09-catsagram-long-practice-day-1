c2250af7c2ed44d58498d87a031de011a6b8ec61
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-12-20 17:46:35 -0800

    crazy cats
    some crazy crazy cats

---
+++ phase-2.md
@@ -0,0 +1,11 @@
+First, add a button that requests and displays a new cat image, replacing the original image.
+
+Second, for the active image, allow the user to upvote/downvote the image and display the score (upvotes - downvotes).
+
+Third, allow the user to add comments to the image. You want to access the value of the comment input field and append a new element representing the comment.
+
+Whenever a new image is requested, the upvotes and comments should be reset.
+
+The final product should at the very least match wireframes/phase-2.png. If you finish early, try to implement a delete comment feature!
+
+By this point, your code could be getting a little bit messy within index.js. Try DRYing up your code and keep in mind the Single Responsibility Principle.
--- public/index.js
+++ public/index.js
@@ -36,6 +36,13 @@ const container = document.createElement("div")
 const img = document.createElement("img")
 const likeBtn = document.createElement("likeBtn")
 const nopeBtn = document.createElement("nopeBtn")
+const newCatBtn = document.createElement("newCatBtn")
+const commentBtn = document.createElement("commentBtn")
+const commentSection = document.createElement("commentSection")
+const commentText = document.createElement("textArea")
+const commentSubmit = document.createElement("commentSubmit")
+const commentClose = document.createElement("commentClose")
+const oldComments = document.createElement("oldComments")
 
 
 const voteBar = document.createElement("div")
@@ -43,6 +50,9 @@ const voteBar = document.createElement("div")
 
 document.body.appendChild(container)
 container.classList.add("container")
+
+document.body.appendChild(oldComments)
+oldComments.classList.add("oldComments")
 
 // container.appendChild(box)
 // box.classList.add("img-container")
@@ -57,22 +67,80 @@ voteBar.classList.add("voteBar")
 
 
 container.appendChild(likeBtn)
-container.appendChild(nopeBtn)
 likeBtn.classList.add("btns")
 likeBtn.classList.add("likeBtn")
+likeBtn.innerText = 'likeBtn';
+
+container.appendChild(nopeBtn)
 nopeBtn.classList.add("btns")
 nopeBtn.classList.add("nopeBtn")
-
-likeBtn.innerText = 'likeBtn';
 nopeBtn.innerText = "nopeBtn"
 
+container.appendChild(commentBtn)
+commentBtn.classList.add("btns")
+commentBtn.classList.add("commentBtn")
+commentBtn.innerText = "Comment"
 
+container.appendChild(newCatBtn)
+newCatBtn.classList.add("btns")
+newCatBtn.classList.add("newCatBtn")
+newCatBtn.innerText = "newCatBtn"
+
+
+container.appendChild(commentSection)
+commentSection.classList.add("commentSection")
+commentSection.classList.add("hidden")
+
+commentSection.appendChild(commentText)
+commentText.classList.add('commentText')
+
+
+commentSection.appendChild(commentSubmit)
+commentSubmit.classList.add("btns")
+commentSubmit.classList.add('commentSubmit')
+commentSubmit.innerText = 'Submit'
+
+
+commentSection.appendChild(commentClose)
+commentClose.classList.add("btns")
+commentClose.classList.add('commentClose')
+commentClose.innerText = 'Close'
+
+commentBtn.addEventListener('click', function () {
+  commentSection.classList.remove("hidden")
+});
+
+commentClose.addEventListener('click', function () {
+  commentSection.classList.add("hidden")
+});
+
+
+commentSubmit.addEventListener('click', function () {
+  const newComment = document.createElement("div")
+  newComment.classList.add('newComment')
+
+  const date = document.createElement("div")
+
+
+  let comment = commentText.value
+
+  commentText.innerText = comment
+
+  newComment.appendChild(date)
+  newComment.appendChild(commentText)
+
+  console.log(comment);
+
+  oldComments.appendChild(newComment)
+
+
+
+});
 
 
 likeBtn.addEventListener('click', function () {
   likes++
   console.log('likes',likes);
-
   changeWidth()
 });
 
@@ -81,6 +149,30 @@ nopeBtn.addEventListener('click', function () {
   console.log('nopes',nopes);
   changeWidth()
 });
+
+nopeBtn.addEventListener('click', function () {
+  nopes++
+  console.log('nopes',nopes);
+  changeWidth()
+});
+
+
+newCatBtn.addEventListener('click', async function () {
+  try{
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const json = await res.json();
+    img.src = json[0].url;
+    likes = 0
+    nopes = 0
+  } catch (err) {
+    console.log('Error!', err.message);
+  }
+});
+
+
+
+
+
 
 
 
@@ -95,17 +187,35 @@ function calcPercent() {
 
 function changeWidth() {
   let p = calcPercent()
+
+  
+
+
+  let totalVotes = likes + nopes
+  let no = nopes/totalVotes
+  let yes = likes/totalVotes
+
+
   // voteBar.style.setProperty('--width', `${p}`);
-  voteBar.style.setProperty('--width', `${p}%`);
+  voteBar.style.setProperty('--widthNo', `${no}%`);
+  voteBar.style.setProperty('--widthYes', `${yes}%`);
 }
 
+  
+
+
+//   // voteBar.style.setProperty('--width', `${p}`);
+//   voteBar.style.setProperty('--width', `${p}%`);
+// }
 
 
 
-if(likes+nopes === 0){
-  console.log('hi');
-  voteBar.style.setProperty('background-color', 'white');
-}
+
+// if(likes+nopes === 0){
+//   console.log('hi');
+//   voteBar.style.setProperty('background-color', 'white');
+// }
+
 
 
 await fetch("https://api.thecatapi.com/v1/images/search")
